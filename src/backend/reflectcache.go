c8bfe6b48c78a94d19243ae01ee060bf4bee3c81
karminski <code.karminski@outlook.com>
2021-04-10 21:40:21 +0800

    UPDATE resolve method.

--- src/backend/reflectcache.go
+++ src/backend/reflectcache.go
@@ -1,5 +1,13 @@
 package backend
 
+import (
+	"sync"
+	"reflect"
+	"errors"
+    "fast-graphql/src/frontend"
+
+)
+
 // A field represents a single field found in a struct.
 type field struct {
 	name          string
@@ -25,8 +33,8 @@ func getCachedTypeFields(t reflect.Type) structFields {
 	return f.(structFields)
 }
 
-func getTypeFields(t reflect.Type) structFields {
-
+func getTypeFields(t reflect.Type) *structFields {
+	return nil
 }
 
 
@@ -58,10 +66,56 @@ func buildBoolField(s Stringifier, field string, value interface{}) {
     s.buildBool(value.(bool))
 }
 
+
+func NewResolveFunctionMap() map[string]interface{} {
+	var fmap map[string]interface{}
+	fmap = make(map[string]interface{})
+	return fmap
+}
+
+
+func buildSchemaResolveFunctionMap(objectFields ObjectFields, fmap map[string]interface{})  {	
+	for fieldName, objectField := range objectFields {
+		if objectField.ResolveFunction != nil {
+			fmap[fieldName] = objectField.ResolveFunction
+		}
+		if subFields := getSubObjectFields(objectField); subFields != nil {
+			buildSchemaResolveFunctionMap(subFields, fmap)
+		}
+	} 
+}
+
 // main JIT resolve method
 func steppingSelectionSet(g *GlobalVariables, request Request, selectionSet *frontend.SelectionSet, objectFields ObjectFields, resolvedData interface{}) (string, error) {
 	fmap := NewResolveFunctionMap()
     buildSchemaResolveFunctionMap(objectFields, fmap)
 
+    return "", nil
 
 }
+
+
+func callResolveFuncByArguments(args []interface{}, fmap map[string]interface{}) (interface{}, error) {
+	var field string
+	var p ResolveParams
+
+
+	// call resolve function
+	if _, ok := fmap[field]; !ok {
+		return nil, errors.New("callResolveFuncByArguments(): unknown field input.")
+	}
+	return fmap[field].(ResolveFunction)(p)
+	
+} 
+
+func callResolveFuncBySource(args []interface{}, fmap map[string]interface{}) (interface{}, error) {
+	var field string
+	var p ResolveParams
+
+
+	// call resolve function
+	if _, ok := fmap[field]; !ok {
+		return nil, errors.New("callResolveFuncBySource(): unknown field input.")
+	}
+	return fmap[field].(ResolveFunction)(p)
+} 
